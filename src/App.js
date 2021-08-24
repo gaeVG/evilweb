19bf1f97fd17fb24d5803b54d199614b61d0a058
Gaev Cenne <gaev@evilparadise.com>
2021-08-24 02:07:14 +0200

    add: footer component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Header from './components/Header/Header';
+import Footer from "./components/Footer";
 
 import Home from "./views/Home";
 import Presentation from "./views/Presentation";
+
 
 const App = () => 
 	<div>
@@ -17,7 +19,10 @@ const App = () =>
 
 				<Route exact path ="/" render = { () => ( <Home />) } />
 				<Route exact path ="/presentation" render = { () => ( <Presentation />) } />
+				<Route path ="/presentation/:society" render = { () => ( <Presentation />) } />
 			</Switch>
+
+			<Footer />
 		</BrowserRouter>
 		
 	</div>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+
+	return(
+		<footer className="container-fluid">
+			<div className="row">
+				
+				<div className="col-4">
+					Col
+				</div>
+				<div className="col-8">
+					Col
+				</div>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
